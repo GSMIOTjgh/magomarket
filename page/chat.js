ad96eb13326e798f0628f6cbca70587c75fbe51f
jgh진건희 <132252115+GSMIOTjgh@users.noreply.github.com>
2023-12-15 15:17:45 +0900

    Update chat.js

--- page/chat.js
+++ page/chat.js
@@ -1,80 +1,137 @@
-const Chat = (function() {
-    const myName = "홍노";
-
-    function init() {
-        // Enter 키를 눌렀을 때 메시지 전송
-        $(document).on('keydown', '#messageInput', function(e) {
-            if (e.keyCode == 13 && !e.shiftKey) {
-                e.preventDefault();
-                sendMessage();
-            }
-        });
-
-        // 전송 버튼 클릭 시 메시지 전송
-        $(document).on('click', '#sendMessageButton', function() {
-            sendMessage();
-        });
+    .chat_wrap .header {
+        font-size: 14px;
+        padding: 15px 0;
+        background: #50c4fa;
+        color: rgb(0, 0, 0);
+        text-align: center;
     }
 
-    function createMessageTag(LR_className, senderName, message) {
-        const chatLi = $('div.chat.format ul li').clone();
-        chatLi.addClass(LR_className);
-        chatLi.find('.sender span').text(senderName);
-        chatLi.find('.message span').text(message);
-        return chatLi;
+    .chat_wrap .chat {
+        position: relative; /* 추가된 부분 */
+        padding-bottom: 120px; /* 조정된 부분 */
+        overflow-x: auto;
+        overflow-y: scroll; /* 스크롤 활성화 */
+        max-height: calc(100vh - 100px); /* 최대 높이 지정 */
+        overflow-x: hidden;
+        overflow-y: hidden;
     }
 
-    function appendMessageTag(LR_className, senderName, message) {
-        const chatLi = createMessageTag(LR_className, senderName, message);
-        $('div.chat:not(.format) ul').append(chatLi);
-        // 스크롤 송출 시간.
-        setTimeout(scrollToBottom, 0);
+    .chat_wrap .chat ul {
+        width: 100%;
+        list-style: none;
     }
 
-    function sendMessage() {
-        const message = $('#messageInput').val();
-        if (message.trim() !== "") {
-            const senderName = myName;
-            const data = {
-                "senderName": senderName,
-                "message": message
-            };
-            resive(data);
-            clearTextarea();
+    .chat_wrap .chat ul li {
+        width: 100%;
+    }
+
+    .chat_wrap .chat ul li.left {
+        text-align: left;
+    }
+
+    .chat_wrap .chat ul li.right {
+        text-align: right;
+    }
+
+    .chat_wrap .chat ul li > div {
+        font-size: 13px;
+    }
+
+    .chat_wrap .chat ul li > div.sender {
+        margin: 15px 40px 0 20px;
+        font-weight: bold;
+    }
+
+    .chat_wrap .chat ul li > div.message {
+        display: inline-block;
+        word-break: break-all;
+        margin: 10px 50px;
+        max-width: 75%;
+        border: 1px solid #888;
+        padding: 10px;
+        border-radius: 5px;
+        background-color: #FCFCFC;
+        color: #555;
+        text-align: left;
+    }
+
+    .chat_wrap .input-div {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        background-color: #FFF;
+        text-align: center;
+        border-top: 1px solid #50c4fa;
+    }
+
+    .chat_wrap .input-div > textarea {
+        width: 82%;
+        height: 80px;
+        border: none;
+        padding: 1px;
+        position: absolute;
+        bottom: 0;
+    }
+
+    .textarea {
+        display: block;
+        width: 100%;
+        height: 65px;
+        border: none;
+        padding: 1px;
+    }
+
+    .format {
+        display: none;
+    }
+
+    .bx{
+        width: 100%;
+        height: 100%;
+    }
+
+    /* textarea의 너비를 버튼에 맞게 조절하지 않고 그대로 유지합니다 */
+    .chat_wrap .input-div .textarea {
+        width: 100%;
+    }
+
+    .upload-button {
+        width: 15%;
+        height: 5%;
+        background-color: #50c4fa;
+        color: #fff;
+        border: none;
+        cursor: pointer;
+        position: absolute;
+        left: 0.7%;
+        bottom: 85px; /* 조정된 위치 */
+    }
+
+    .exit{
+        bottom: 85px;
+        color: #fff;
+        border: none;
+        cursor: pointer;
+        position: absolute;
+        left: 17%;
+    }
+
+    /* 작은 화면에 대한 반응형 스타일 추가 */
+    @media (max-width: 600px) {
+        .upload-button {
+            width: 80%;
+            height: 8%;
+            top: calc(50vh - (40vh / 2)); /* 조정된 위치 */
+            left: 10%;
         }
     }
 
-    function clearTextarea() {
-        $('#messageInput').val('');
+    /* 중간 크기 화면에 대한 반응형 스타일 추가 */
+    @media (max-width: 768px) {
+        .upload-button {
+            width: 60%;
+            height: 8%;
+            top: calc(60vh - (48vh / 2)); /* 조정된 위치 */
+            left: 10%;
+        }
     }
-
-    function resive(data) {
-        const LR = (data.senderName != myName) ? "left" : "right";
-        appendMessageTag(LR, data.senderName, data.message);
-    }
-
-    return {
-        'init': init
-    };
-})();
-
-$(function(){
-    Chat.init();
-});
-
-function scrollToBottom() {
-    const chatDiv = document.querySelector('.chat');
-    if (chatDiv) {
-        chatDiv.scrollTop = chatDiv.scrollHeight;
-    }
-}
-
-// MutationObserver를 생성하고 채팅창을 감시.
-const chatDiv = document.querySelector('.chat');
-const observer = new MutationObserver((mutationsList) => {
-    // 스크롤이 변화하면 최하단으로 이동.
-    scrollToBottom();
-});
-
-// MutationObserver 시작 코드.
-observer.observe(chatDiv, { childList: true });
